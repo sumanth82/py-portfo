3b06bd8ad3d79138f66010ecaf11fc32b23e8142
sumanth82 <sumanth82@gmail.com>
2020-11-05 02:45:43 -0500

    Updates

--- portfolio.py
+++ portfolio.py
@@ -2,68 +2,79 @@ import flask
 import csv
 
 from flask import url_for, Flask, render_template, request, redirect
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def my_home():
+    # render_template method looks for the files in templates/folder
     return render_template('index.html')
 
-@app.route('/<string:page_name>')             # With this single entry, you can comment out the rest of the lines
+
+# With this single entry, you can comment out the rest of the lines
+@app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
 
 def write_to_file(data):
-    with open('database.txt', mode='a') as database:                                  # Create a function to store the data rxed in forms in database.txt
+    # Create a function to store the data rxed in forms in database.txt
+    with open('database.txt', mode='a') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        file=database.write(f'\n {email}, {subject}, {message}')
+        file = database.write(f'\n {email}, {subject}, {message}')
 
 
 # Instead of the above function, where the data is written to a txt file, let's write to a csv file:
 
 def write_to_csv(data):
-    with open('database.csv', newline='', mode='a') as database2:                                  # Create a function to store the data rxed in forms in database.txt
+    # Create a function to store the data rxed in forms in database.txt
+    with open('database.csv', newline='', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer=csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
-@app.route('/submit_form', methods=['POST', 'GET']) # For this to work, make some changes to the contact.html under <send> tab# See changes in line 75 and 62
-#def submit_form():
+
+# For this to work, make some changes to the contact.html under <send> tab# See changes in line 75 and 62
+@app.route('/submit_form', methods=['POST', 'GET'])
+# def submit_form():
 #    return 'Form Submitted! Thank you!'
-def get_data():                                  # Use this function to get the contact data posted by the e-mail client
+# Use this function to get the contact data posted by the e-mail client
+def get_data():
     if request.method == 'POST':
         try:
-            data=request.form.to_dict()
-            #write_to_file(data)                     # Call this function to store the data in database.txt
+            data = request.form.to_dict()
+            # write_to_file(data)                     # Call this function to store the data in database.txt
             write_to_csv(data)                     # Let's try to write to csv
             print(data)
-            return redirect('/thankyou.html')       # import redirect method for this 
+            # import redirect method for this
+            return redirect('/thankyou.html')
         except:
-            return: 'did not save to database'
+            return 'did not save to database'
     else:
         return 'something went wrong'
 
 
-#@app.route('/works.html')
-#def works():
+# @app.route('/works.html')
+# def works():
 #    return render_template('works.html')
 
-#@app.route('/work.html')
-#def work():
+# @app.route('/work.html')
+# def work():
 #    return render_template('work.html')
 
-#@app.route('/about.html')
-#def about_us():
+# @app.route('/about.html')
+# def about_us():
 #    return render_template('about.html')
 
-#@app.route('/contact.html')
-#def contact():
+# @app.route('/contact.html')
+# def contact():
 #    return render_template('contact.html')
 
-#@app.route('/components.html')
-#def component():
+# @app.route('/components.html')
+# def component():
 #    return render_template('components.html')
---
+++ readme.md
@@ -0,0 +1,18 @@
+## STEPS TO RUN THIS WEB-APP
+
+cd ./
+python3 -m venv venv
+pip3 install flask
+
+export FLASK_ENV=development
+export FLASK_APP=portfolio.py
+flask run 
+
+access on localhost: http://127.0.0.1:5000/
+
+### Helpful Links:
+
+https://swapi.dev/ - For Star Wars API
+
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types - HTTP Content Type Header Details 
+
